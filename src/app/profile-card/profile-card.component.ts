873203c34b20b5d3df19c1546a3b2914ba886deb
nikohysa <nikohysa@unyt.edu.al>
2018-11-24 14:36:37 +0100

    made it cool

---
+++ src/app/profile-card/profile-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  templateUrl: './profile-card.component.html',
+  styleUrls: ['./profile-card.component.less']
+})
+export class ProfileCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
